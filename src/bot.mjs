85bc669d2fef50b78312d0e113f433c93d205206
maratmut <103142293+maratmut@users.noreply.github.com>
2023-05-26 10:52:02 +0300

    fixing

--- src/bot.mjs
+++ src/bot.mjs
@@ -3,19 +3,24 @@ import TeleBot from "telebot"
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 // Обрабатываем событие start
-bot.on(['/start'], async (msg) => {
-    // Отправляем приветственное сообщение
-    await bot.sendMessage(msg.chat.id, `Привет, ${msg.from.first_name}!`);
+bot.on(['/start'], (msg) => {
+  // Отправляем запрос на подтверждение подписки на канал
+  bot.api.call('restrictChatMember', {
+    chat_id: '-1001380460033',
+    user_id: msg.from.id,
+    can_send_messages: true,
+    can_send_media_messages: true,
+    can_send_other_messages: true,
+    can_add_web_page_previews: true,
+    until_date: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 365 // На год вперед
+  }).then(() => {
+    // Пишем сообщение об успешной подписке
+    bot.sendMessage(msg.chat.id, 'Вы успешно подписались на наш канал!');
+  }).catch((error) => {
+    console.error(error);
+    bot.sendMessage(msg.chat.id, 'Произошла ошибка при подписке. Попробуйте позже.');
   });
-  
-  // Обрабатываем событие подписки на канал
-  bot.on('new_chat_members', async (msg) => {
-    // Проверяем, что пользователь подписался на нужный канал
-    if (msg.chat.username === 'название_канала') {
-      // Отправляем сообщение о подтверждении подписки
-      await bot.sendMessage(msg.chat.id, `Спасибо за подписку, ${msg.from.first_name}!`);
-    }
-  });
+});
   
 
 export default bot
